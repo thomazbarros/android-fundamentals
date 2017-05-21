fb5401439534b8ea5ae3468d49f352788fb47b65
Thomaz Barros <thomazavila@gmail.com>
2017-05-21 10:13:16 -0300

    Chapter 3.10.2b - Setting up the basics for the project.

--- google_android_book/cap3/Cap3.10.2b/app/src/main/java/thomaz/barros/cap3102b/MainActivity.java
+++ google_android_book/cap3/Cap3.10.2b/app/src/main/java/thomaz/barros/cap3102b/MainActivity.java
@@ -2,6 +2,14 @@ package thomaz.barros.cap3102b;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/*
+Treating events with ...
+* */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bLogin = (Button) findViewById(R.id.bLogin);
+        bLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                EditText etUsername = (EditText) findViewById(R.id.etUsername);
+                EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString();
+
+                if(username.equals("thomaz")&& password.equals("123")){
+                    alert("Welcome!");
+                }
+                else {
+                    alert("Wrong credentials.");
+                }
+
+            }
+        });
+
+
+    }
+
+    private void alert (String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
